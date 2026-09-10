7eb00841fc5141cad6f4bbb2059dc37c65a6e82a
GuilhermeRVenturini <gventurini@marlin.com.br>
2026-09-10 10:07:18 -0300

    Result Pattern
    Adições:
    - Result.cs
    - IRepository
    - Repository

---
+++ Infrastructure.ListaCompras/Repositories/Repository.cs
@@ -0,0 +1,18 @@
+﻿using Infrastructure.ListaCompras.Common.Results;
+using Infrastructure.ListaCompras.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.ListaCompras.Repositories
+{
+    public class Repository<T, TId> : IRepository<T, TId> where T : class
+    {
+        private readonly ListaCompras_DbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public Repository(ListaCompras_DbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+    }
+}
