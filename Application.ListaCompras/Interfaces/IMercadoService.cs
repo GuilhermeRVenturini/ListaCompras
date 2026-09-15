8c0bc4e34cdca2fe4f163b633433c7caf4b1da99
GuilhermeRVenturini <gventurini@marlin.com.br>
2026-09-15 14:29:32 -0300

    Services
    Service do Mercado

---
+++ Application.ListaCompras/Interfaces/IMercadoService.cs
@@ -0,0 +1,18 @@
+﻿using Domain.ListaCompras.Common.Results;
+using Domain.ListaCompras.Entities;
+
+namespace Application.ListaCompras.Interfaces
+{
+    public interface IMercadoService
+    {
+        Task<ResultData<IEnumerable<Mercado>>> GetAllAsync();
+
+        Task<ResultData<Mercado>> GetByIdAsync(int id);
+
+        Task<ResultData<Mercado>> CreateAsync(Mercado mercado);
+
+        Task<ResultData<Mercado>> UpdateAsync(Mercado mercado);
+
+        Task<ResultData<Mercado>> DeleteAsync(int id);
+    }
+}
---
+++ Application.ListaCompras/Services/MercadoService.cs
@@ -0,0 +1,42 @@
+﻿using Application.ListaCompras.Interfaces;
+using Domain.ListaCompras.Common.Results;
+using Domain.ListaCompras.Entities;
+using Domain.ListaCompras.Interfaces;
+
+namespace Application.ListaCompras.Services
+{
+    public sealed class MercadoService : IMercadoService
+    {
+        private readonly IRepository<Mercado, int> _repository;
+
+        public MercadoService(IRepository<Mercado, int> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<ResultData<IEnumerable<Mercado>>> GetAllAsync()
+        {
+            return await _repository.GetAllAsync();
+        }
+
+        public async Task<ResultData<Mercado>> GetByIdAsync(int id)
+        {
+            return await _repository.GetByIdAsync(id);
+        }
+
+        public async Task<ResultData<Mercado>> CreateAsync(Mercado mercado)
+        {
+            return await _repository.CreateAsync(mercado);
+        }
+
+        public async Task<ResultData<Mercado>> UpdateAsync(Mercado mercado)
+        {
+            return await _repository.UpdateAsync(mercado);
+        }
+
+        public async Task<ResultData<Mercado>> DeleteAsync(int id)
+        {
+            return await _repository.DeleteAsync(id);
+        }
+    }
+}
